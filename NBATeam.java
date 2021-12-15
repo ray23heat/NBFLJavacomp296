10a1e472b2b85acf92d8655244d0701a898f3c08
ray23heat <73254554+ray23heat@users.noreply.github.com>
2021-12-15 16:34:26 -0500

    Add files via upload

---
+++ NBATeam.java
@@ -0,0 +1,29 @@
+package sportFans;
+
+
+
+public class NBATeam {
+
+	String name;
+	String coach;
+	
+	
+  public NBATeam (String name, String coach) {
+	  this.name = name;
+	  this.coach = coach;
+	
+	  
+  }
+  public String getName() {
+	  return this.name;
+	  
+  }
+  public String getCoach() {
+	  
+	  return this.coach;
+  }
+
+}
+  
+	 
+
